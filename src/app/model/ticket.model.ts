dbe237afd0eb8c04c9a14a76444e5aa028b86e49
Alan Gomes Coimbra <algcoimbra01@gmail.com>
2019-10-19 18:12:13 -0300

    Criando Model e Service do Ticket

---
+++ src/app/model/ticket.model.ts
@@ -0,0 +1,16 @@
+import { User } from './user.model';
+
+export class Ticket{
+    constructor(
+        public id: string,
+        public number: number,
+        public title: string,
+        public status: string,
+        public priority: string,
+        public imagens: string,
+        public user: User,
+        public assingnedUser: User,
+        public data: string,
+        public changes: Array<string>
+    ){}
+}
