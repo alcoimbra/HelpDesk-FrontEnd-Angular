2f2c8ecb8718ac05e95027605ffd8bcd8afacea0
Alan Gomes Coimbra <algcoimbra01@gmail.com>
2019-10-24 09:02:02 -0300

    Implementacao dos Components UserList tanto no .html como no .ts, e fazendo algumas alteracoes no Router/Module/Menu/UserNew.

--- src/app/app,routes.ts
+++ src/app/app,routes.ts
@@ -4,11 +4,14 @@ import { Routes, RouterModule } from '@angular/router'
 import { ModuleWithProviders } from '@angular/core';
 import { AuthGuard } from './components/security/auth.guard';
 import { UserNewComponent } from './components/user-new/user-new.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
-    { path: 'user-new', component: UserNewComponent, canActivate: [AuthGuard] }
+    { path: 'user-new', component: UserNewComponent, canActivate: [AuthGuard] },
+    { path: 'user-new/:id', component: UserNewComponent, canActivate: [AuthGuard] },
+    { path: 'user-list', component: UserListComponent, canActivate: [AuthGuard] }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
